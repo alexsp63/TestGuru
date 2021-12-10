e9dda01229e901ba145026d466e0e3039ae68ecd
Sofya <sofya.popowa@yandex.ru>
2021-12-11 00:12:47 +0300

    part3 task

--- db/migrate/20211210210955_create_questions.rb
+++ db/migrate/20211210210955_create_questions.rb
@@ -1,7 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
-      t.string :text
+      t.string :text, null: false
+      t.references :test, null: false, foreign_key: true
 
       t.timestamps
     end
