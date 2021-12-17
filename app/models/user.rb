11cfaf639482d625abf4e3b78a2cef00a979cebf
Sofya <sofya.popowa@yandex.ru>
2021-12-18 01:02:15 +0300

    part5 task N1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :tests, dependent: :destroy
   has_many :tests_users
   has_many :tests, through: :tests_users, dependent: :destroy
+  has_many :tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
   def all_tests_by_level(level)
     tests.where(level: level)
