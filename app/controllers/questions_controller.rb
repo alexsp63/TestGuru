9124e8731fd6a7254f28e949f773f64ac2f80067
Sofya <sofya.popowa@yandex.ru>
2021-12-24 22:50:52 +0300

    part7 task

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,41 @@
+class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index; end
+
+  def show; end
+
+  def new; end
+
+  def create
+    question = @test.questions.new(question_params)
+    question.save
+    render plain: question.inspect
+  end
+
+  def destroy
+    deleted_question = @question.destroy
+    render plain: deleted_question.inspect
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title)
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'No question found'
+  end
+end
