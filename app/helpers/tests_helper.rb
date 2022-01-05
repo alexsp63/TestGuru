c6fa4d2a0515612c2e193646e5b83a0756e2f4bc
Sofya <sofya.popowa@yandex.ru>
2022-01-06 02:14:25 +0300

    part8 task 1st try

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,14 @@
 module TestsHelper
+
+  def test_level(test)
+    case test.level
+    when 0..1
+      :easy
+    when 2..4
+      :middle
+    when 5..Float::INFINITY
+      :difficult
+    end
+  end
+
 end
