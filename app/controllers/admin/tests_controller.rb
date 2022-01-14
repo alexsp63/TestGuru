e1993017fd805b1516c7d30d5a24febfbfdfa7fd
Sofya <sofya.popowa@yandex.ru>
2022-01-14 22:28:29 +0300

    all videos

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,60 @@
+class Admin::TestsController < Admin::BaseController
+  before_action :find_test, only: %i[show destroy edit update start]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    @questions = @test.questions
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit; end
+
+  def create
+    @test = current_user.authored_tests.build(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
+  end
+
+  def start
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
+
+  def rescue_with_test_not_found
+    render plain: 'No test found'
+  end
+end
